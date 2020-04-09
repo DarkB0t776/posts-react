99cfa19b4fd85f2caa2604c5df597e8917882675
Dima <dima23790@gmail.com>
2020-04-09 21:02:37 +0300

    Updates

---
+++ precache-manifest.eac5eb69c8aba3a23d0e1416ad7b9d39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d158f01d01c17b923f002a586d2b5f6f",
+    "url": "/posts-react/index.html"
+  },
+  {
+    "revision": "48333d635109b53df595",
+    "url": "/posts-react/static/css/main.4120f77b.chunk.css"
+  },
+  {
+    "revision": "bdb8b3f4bdf37d5832c3",
+    "url": "/posts-react/static/js/2.58decb6f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/posts-react/static/js/2.58decb6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48333d635109b53df595",
+    "url": "/posts-react/static/js/main.3a87634a.chunk.js"
+  },
+  {
+    "revision": "151c76a1ae284a68c20f",
+    "url": "/posts-react/static/js/runtime-main.2b729285.js"
+  }
+]);
